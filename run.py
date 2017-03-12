d6630a779142ca20c220f3af40c89236df9deb50
Pikalovs <mareks.pikalovs@accenture.com>
2017-03-12 18:50:34 +0200

    Added PORT variable for Heroku deployment

--- run.py
+++ run.py
@@ -1,2 +1,6 @@
 from app import app
-app.run(debug=True,host='0.0.0.0')
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    app.run(debug=True, host='0.0.0.0')
