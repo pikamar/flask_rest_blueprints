4f26ea4221491cc0fb2f2d276f7256ee3afab793
Pikalovs <mareks.pikalovs@accenture.com>
2017-03-22 09:23:09 +0200

    Posrt 5000 changed to 8080

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import os
 if os.environ.get('APP_LOCATION') == 'heroku':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
